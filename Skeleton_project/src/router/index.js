41f85f4f52d8173a092b9e909308aa5dc290487e
ssooomm <fivesoomin@gmail.com>
2024-06-10 17:12:40 +0900

    라우터 적용

--- Skeleton_project/src/router/index.js
+++ Skeleton_project/src/router/index.js
@@ -1,9 +1,13 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from "@/pages/Dashboard.vue";
+import Record from "@/pages/Record.vue";
+import Chart from "@/pages/Chart.vue";
 
 const routes = [
     { path: '/', component: Dashboard },
+    { path: '/record', component: Record },
+    { path: '/chart', component: Chart },
 ];
 
 const router = createRouter({
