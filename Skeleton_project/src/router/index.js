dcf4a9c0d6f1efd7e2fae352779ac04625068fed
isj0228 <isjsjd0228@gmail.com>
2024-06-10 17:51:43 +0900

    userprofile

--- Skeleton_project/src/router/index.js
+++ Skeleton_project/src/router/index.js
@@ -1,9 +1,11 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from "@/pages/Dashboard.vue";
+import UserProfile from "@/pages/UserProfile.vue"
 
 const routes = [
     { path: '/', component: Dashboard },
+    { path: '/User', component: UserProfile}
 ];
 
 const router = createRouter({
