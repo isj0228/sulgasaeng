4d9ce7a220abf240fd806fa2bdb85383e69151b2
ssooomm <fivesoomin@gmail.com>
2024-06-10 16:29:30 +0900

    bootstrap파일과 router 연결

--- Skeleton_project/src/main.js
+++ Skeleton_project/src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-// import './style.css'
-import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index.js';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
