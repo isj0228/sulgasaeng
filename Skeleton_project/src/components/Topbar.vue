097db22e2156d6cccc8b24fdfc0b3fcf025f3a03
ssooomm <fivesoomin@gmail.com>
2024-06-11 10:23:06 +0900

    topbar에 드롭다운 삭제

--- Skeleton_project/src/components/Topbar.vue
+++ Skeleton_project/src/components/Topbar.vue
@@ -162,36 +162,14 @@
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
+            <!-- 프로필사진으로 수정할 부분 to, span, img-->
             <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
+                <router-link class="nav-link dropdown-toggle" to="/chart">
                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                    <img class="img-profile rounded-circle" src="/img/undraw_profile.svg">
-                </a>
-                <!-- Dropdown - User Information -->
-                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Profile
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Settings
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Activity Log
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Logout
-                    </a>
-                </div>
+                    <img class="img-profile rounded-circle" src="/img/undraw_profile.svg"> 
+                </router-link>
             </li>
-
         </ul>
-
     </nav>
     <!-- End of Topbar -->
 </template>
