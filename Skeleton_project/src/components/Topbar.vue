3d1e859729613cf046783be5841e9b58a6d1fa1f
unknown <isjsjd0228@gmail.com>
2024-06-11 12:42:02 +0900

    done

--- Skeleton_project/src/components/Topbar.vue
+++ Skeleton_project/src/components/Topbar.vue
@@ -1,71 +1,25 @@
 <template>
-    <!-- Topbar -->
     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-        <!-- Sidebar Toggle (Topbar) -->
         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
         </button>
 
-        <!-- Topbar Search -->
-        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                    aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-
-        <!-- Topbar Navbar -->
         <ul class="navbar-nav ml-auto">
 
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-search fa-fw"></i>
-                </a>
-                <!-- Dropdown - Messages -->
-                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                    aria-labelledby="searchDropdown">
-                    <form class="form-inline mr-auto w-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </li>
-
-            <!-- Nav Item - Alerts -->
             <li class="nav-item dropdown no-arrow mx-1">
                 <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-bell fa-fw"></i>
-                    <!-- Counter - Alerts -->
                     <span class="badge badge-danger badge-counter">3+</span>
                 </a>
-                <!-- Dropdown - Alerts -->
-                <!-- Dropdown content removed for brevity -->
             </li>
 
-            <!-- Nav Item - Messages -->
             <li class="nav-item dropdown no-arrow mx-1">
-                <!-- Dropdown content removed for brevity -->
             </li>
 
             <div class="topbar-divider d-none d-sm-block"></div>
 
-            <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
                 <router-link class="nav-link dropdown-toggle" to="/user">
                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
@@ -74,14 +28,12 @@
             </li>
         </ul>
     </nav>
-    <!-- End of Topbar -->
 </template>
 
 <script>
 import $ from 'jquery';
 export default {
     mounted() {
-        // Toggle the side navigation
         $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
             $("body").toggleClass("sidebar-toggled");
             $(".sidebar").toggleClass("toggled");
