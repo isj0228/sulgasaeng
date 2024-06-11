774112ccb71d525f98255c30adbf0ce45d5f40e0
subinPark <subin4420@gmail.com>
2024-06-11 13:13:48 +0900

    update recode1 recode

--- Skeleton_project/src/pages/Record1.vue
+++ Skeleton_project/src/pages/Record1.vue
@@ -1,26 +1,29 @@
 <template>
     <div>
       <h1>내역조회</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Category</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="transaction in recentTransactions" :key="transaction.id">
-            <td>{{ transaction.date }}</td>
-            <td>{{ transaction.type }}</td>
-            <td>{{ transaction.amount }}</td>
-            <td>{{ transaction.category }}</td>
-            <td>{{ transaction.desc }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="transaction in recentTransactions" :key="transaction.id">
+              <td>{{ transaction.date }}</td>
+              <td>{{ transaction.type }}</td>
+              <td>{{ transaction.amount }}</td>
+              <td>{{ transaction.category }}</td>
+              <td>{{ transaction.desc }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </template>
   
@@ -31,6 +34,7 @@
   export default defineComponent({
     setup() {
       const budgetStore = useBudgetStore()
+      const loading = ref(true)
   
       // 최근 5개의 거래 내역을 계산하는 computed 속성
       const recentTransactions = computed(() => {
@@ -39,11 +43,16 @@
   
       // onMounted 훅 사용하여 컴포넌트가 DOM에 마운트된 후에 데이터를 가져옵니다.
       onMounted(async () => {
-        await budgetStore.getTransactions() // 데이터 가져오기
+        try {
+          await budgetStore.getTransactions() // 데이터 가져오기
+        } finally {
+          loading.value = false
+        }
       })
   
       return {
         recentTransactions,
+        loading
       }
     }
   })
