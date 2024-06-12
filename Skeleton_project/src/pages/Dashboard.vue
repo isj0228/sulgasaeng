a05b9f7b25f06f6dc0359e74b6949d35ccff52c6
ssooomm <fivesoomin@gmail.com>
2024-06-12 14:06:27 +0900

    제목 제거

--- Skeleton_project/src/pages/Dashboard.vue
+++ Skeleton_project/src/pages/Dashboard.vue
@@ -3,11 +3,11 @@
     <h2>대시보드</h2>
     <Record />
   </div> -->
-  <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-        class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
-  </div>
+  <!-- <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">모아보기</h1>
+    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+        class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+  </div> -->
 
   <div class="row">
     <div class="col-xl-3 col-md-6 mb-4">
