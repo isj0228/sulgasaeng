fc89d650dfa4bdb85e0a1d442a19c12ec943a93d
wxxhyeong <wxxhyeong@gmail.com>
2024-06-13 17:47:57 +0900

    feat: login.js 등등

--- Skeleton_project/src/pages/Login.vue
+++ Skeleton_project/src/pages/Login.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        <center>
+        <form @submit.prevent="checkLogin" >
             <h1>Login</h1>
             <br>
-            ID<input type="text" >
-            PW<input type="password">
-        </center>
+            ID   &nbsp;&nbsp;<input type="text" value="" v-model="inputID">
+            <br>
+            <br>
+            PW <input type="password" value="" v-model="inputPW">
+            <br><br>
+            <button type="submit" class="btn btn-primary btn-user btn-block">로그인</button>
+        </form>
     </div>
 </template>
 
 <script setup>
+import { useLoginStateStore } from '../stores/loginStateStore';
+import { useUserStore } from '../stores/userStore';
+
+const inputID = "";
+const inputPW = "";
+const loginStore = useLoginStateStore();
+const userStore = useUserStore();
+const checkLogin = function() {
+    
+
+}
 
 
 </script>
--- Skeleton_project/src/router/index.js
+++ Skeleton_project/src/router/index.js
@@ -1,20 +1,34 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from "@/pages/Dashboard.vue";
 import Record from "@/pages/Record.vue";
 import Chart from "@/pages/Chart.vue";
 import UserProfile from "@/pages/UserProfile.vue";
+import Login from "@/pages/Login.vue";
+import { useLoginStateStore } from '../stores/loginStateStore';
 
 const routes = [
     { path: '/user', component: UserProfile},
     { path: '/', component: Dashboard },
     { path: '/record', component: Record },
     { path: '/chart', component: Chart },
+    { path: '/login', component: Login}
 ];
+
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    const authStore = useLoginStateStore();
+    if (to.path != '/login' && !authStore.loginState) {
+        next('/login');
+    }
+    else {
+        next();
+    }
+})
 
 export default router;
